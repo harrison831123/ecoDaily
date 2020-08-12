48b66ad917cad49d12197541e116ed4b3b66e9de
Harrison <harrison831123@gmail.com>
2020-08-12 10:51:50 +0800

    .

---
+++ ecoBack/slnecoBack/ecoBack/Controllers/BackFinancialController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ecoBack.Controllers
+{
+    public class BackFinancialController : Controller
+    {
+        // GET: BackFinancial
+        public ActionResult managementFee()
+        {
+            return View();
+        }
+        public ActionResult incomeList()
+        {
+            return View();
+        }
+        public ActionResult incomeKeyIn()
+        {
+            return View();
+        }
+        public ActionResult incomeEdit()
+        {
+            return View();
+        }
+
+        public ActionResult payKeyIn()
+        {
+            return View();
+        }
+        public ActionResult payList()
+        {
+            return View();
+        }
+        public ActionResult financialList()
+        {
+            return View();
+        }
+        public ActionResult gasData()
+        {
+            return View();
+        }
+    }
+}
---
+++ ecoBack/slnecoBack/ecoBack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ecoBack.MvcApplication" Language="C#" %>
